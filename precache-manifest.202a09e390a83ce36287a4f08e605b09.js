6a87ba77e7927988b046e28603f7032a795d7dcb
Pawel Bareja <bareja.pawel@gmail.com>
2019-07-17 15:36:47 +0200

    Updates

---
+++ precache-manifest.202a09e390a83ce36287a4f08e605b09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aadf1a45344e94e78fa6f77007e9ce3",
+    "url": "/spa-react/index.html"
+  },
+  {
+    "revision": "e45a89778e4f74b30896",
+    "url": "/spa-react/static/css/main.fdec7e1f.chunk.css"
+  },
+  {
+    "revision": "7d1de852471b8c00ef13",
+    "url": "/spa-react/static/js/2.12f9b2ad.chunk.js"
+  },
+  {
+    "revision": "e45a89778e4f74b30896",
+    "url": "/spa-react/static/js/main.e9bf37c9.chunk.js"
+  },
+  {
+    "revision": "2dc989bf8d14dd7f584b",
+    "url": "/spa-react/static/js/runtime~main.afad200a.js"
+  },
+  {
+    "revision": "517b76482c689f311e8713958664dfcb",
+    "url": "/spa-react/static/media/foto1.517b7648.png"
+  },
+  {
+    "revision": "4f6e7df57e870911195e461ce9440bd8",
+    "url": "/spa-react/static/media/header.4f6e7df5.jpg"
+  }
+]);
